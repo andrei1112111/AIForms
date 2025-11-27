67a4b71ffbd8287d204c98dc9b64a808989fffb0
andrei1112111 <atishkin060705@gmail.com>
2025-11-27 14:16:12 +0700

    хардкод гугл апи перенесен в .env

--- src/config/config_models.py
+++ src/config/config_models.py
@@ -7,6 +7,17 @@ class DeepSeekConfigModel(BaseModel):
     baseurl: str
     model_type: str
 
+class GoogleAPIs(BaseModel):
+    """моделька под подключение к api google'а"""
+    google_client_id: str
+    google_client_secret: str
+    redirect_url: str
+
+class FlaskServer(BaseModel):
+    """моделька под настройки фласка"""
+    host: str
+    port: int
+
 class PostgresConfigModel(BaseModel):
     """моделька под подключение к бд через psycpg"""
     host: str
@@ -23,6 +34,8 @@ class ConfigModel(BaseModel):
     PostgreSQL: PostgresConfigModel
     DeepSeek: DeepSeekConfigModel
     Logger: LoggingConfigModel
+    FlaskServer: FlaskServer
+    GoogleAPIs: GoogleAPIs
  
     model_config = { # pydantic v2
         "arbitrary_types_allowed": True
